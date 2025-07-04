81028f23da65862413d585aab3d2149d8bdd89f3
shivraj pandit <panditshivraj934@gmail.com>
2025-07-04 21:39:44 +0530

    third

---
+++ Example.java
@@ -0,0 +1,15 @@
+public class Example {
+    public static void main(String[] args) {
+        int age = 25;
+        double price = 19.99;
+        char grade = 'A';
+        String name = "Alice";
+        boolean isJavaFun = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Price: $" + price);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
